b519010606111ac35d5f7460b4645b912b5fed5b
ahn1305 <ahnashwin1305@gmail.com>
2021-06-25 09:59:22 +0000

    latest changes

--- blogapp/models.py
+++ blogapp/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.conf import settings
 
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
